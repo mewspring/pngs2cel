706e4dc4d4be57352bcbb74ba3a304f20e9b4c0d
Robin Eklind <rnd0x00@gmail.com>
2020-08-07 02:29:22 +0200

    pngs2cel: add -cl2 flag to store output in CL2 format (preliminary support)

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"image/color"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/mewkiz/pkg/imgutil"
 	"github.com/pkg/errors"
@@ -25,11 +26,14 @@ Usage: pngs2cel [OPTIONS]... FILE.png...`
 func main() {
 	// Parse command line arguments.
 	var (
+		// Store output in CL2 format.
+		cl2 bool
 		// CEL image output path.
 		output string
 		// Path to levels/towndata/town.pal.
 		palPath string
 	)
+	flag.BoolVar(&cl2, "cl2", false, "store output in Cl2 format")
 	flag.StringVar(&output, "o", "output.cel", "CEL image output path")
 	flag.StringVar(&palPath, "pal_path", "town.pal", "path to levels/towndata/town.pal")
 	flag.Usage = usage
@@ -40,7 +44,7 @@ func main() {
 		os.Exit(1)
 	}
 	// Convert PNG images to a single CEL image.
-	if err := pngs2cel(pngPaths, output, palPath); err != nil {
+	if err := pngs2cel(pngPaths, output, palPath, cl2); err != nil {
 		log.Fatalf("%+v", err)
 	}
 }
@@ -48,7 +52,7 @@ func main() {
 // pngs2cel converts the given PNG images to a single CEL image, writing the
 // output CEL file to the specified output path and parsing the town.pal colour
 // palette from the specified PAL path.
-func pngs2cel(pngPaths []string, output, palPath string) error {
+func pngs2cel(pngPaths []string, output, palPath string, cl2 bool) error {
 	// Parse town.pal.
 	pal, err := cel.ParsePal(palPath)
 	if err != nil {
@@ -64,8 +68,17 @@ func pngs2cel(pngPaths []string, output, palPath string) error {
 		imgs = append(imgs, img)
 	}
 	// Convert PNG images to CEL image.
-	celImg := createCel(imgs, pal)
+	var celImg celImage
+	switch {
+	case cl2:
+		celImg = createCl2(imgs, pal)
+	default:
+		celImg = createCel(imgs, pal)
+	}
 	// Write CEL image to file.
+	if cl2 && strings.HasSuffix(output, ".cel") {
+		output = strings.TrimSuffix(output, ".cel") + ".cl2"
+	}
 	if err := dumpCel(celImg, output); err != nil {
 		return errors.WithStack(err)
 	}
@@ -92,6 +105,30 @@ func createCel(imgs []image.Image, pal color.Palette) celImage {
 	var frames [][]byte
 	for _, img := range imgs {
 		frame := getCelFrame(img, pal)
+		frames = append(frames, frame)
+	}
+	frameOffsets := make([]uint32, nframes+1)
+	frameOffsets[0] = 4 + 4*uint32(len(frameOffsets))
+	for i, frame := range frames {
+		frameOffsets[i+1] = uint32(len(frame)) + frameOffsets[i]
+	}
+	celImg := celImage{
+		nframes:      uint32(nframes),
+		frameOffsets: frameOffsets,
+		frames:       frames,
+	}
+	return celImg
+}
+
+// createCl2 creates a CL2 image based on the given image frames and colour
+// palette.
+func createCl2(imgs []image.Image, pal color.Palette) celImage {
+	nframes := len(imgs)
+	var frames [][]byte
+	for _, img := range imgs {
+		frame := getCl2Frame(img, pal)
+		header := []byte{0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+		frame = append(header, frame...)
 		frames = append(frames, frame)
 	}
 	frameOffsets := make([]uint32, nframes+1)
@@ -148,6 +185,56 @@ func getCelFrame(img image.Image, pal color.Palette) []byte {
 				continue
 			}
 			if ntrans >= 0x80 || (ntrans > 0 && lastPixelOnRow) {
+				setTrans()
+				continue
+			}
+		}
+	}
+	return frame
+}
+
+// getCl2Frame converts the given image to the corresponding CL2 frame contents,
+// using the specified palette for colours.
+func getCl2Frame(img image.Image, pal color.Palette) []byte {
+	bounds := img.Bounds()
+	var frame []byte
+	ntrans := 0       // transparent pixels.
+	var pixels []byte // regular pixels.
+	// Set regular pixels.
+	setRegular := func() {
+		cmd := byte(-len(pixels))
+		frame = append(frame, cmd)
+		frame = append(frame, pixels...)
+		pixels = pixels[:0] // reset pixel buffer.
+	}
+	// Set transparent pixels.
+	setTrans := func() {
+		t := byte(ntrans)
+		frame = append(frame, t)
+		ntrans = 0
+	}
+	for y := bounds.Max.Y - 1; y >= 0; y-- {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			c := img.At(x, y)
+			if isTransparent(c) {
+				if len(pixels) > 0 {
+					setRegular()
+				}
+				ntrans++
+			} else {
+				if ntrans > 0 {
+					setTrans()
+				}
+				idx := byte(pal.Index(c))
+				pixels = append(pixels, idx)
+			}
+			lastPixel := x == bounds.Max.X-1 && y == bounds.Max.Y-1
+			// -1 through -65
+			if len(pixels) >= 65 || (len(pixels) > 0 && lastPixel) {
+				setRegular()
+				continue
+			}
+			if ntrans >= 0x7F || (ntrans > 0 && lastPixel) {
 				setTrans()
 				continue
 			}
