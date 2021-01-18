f50da1ec4f5b10aef552af3491ab2dd22a5503a3
Robin Eklind <rnd0x00@gmail.com>
2021-01-18 05:10:13 +0100

    cl2: add support for RLE-encoding and CL2 archives

--- README.md
+++ README.md
@@ -3,17 +3,41 @@
 ## Installation
 
 ```
-go get github.com/mewspring/pngs2cel
+git clone https://github.com/mewspring/pngs2cel
+cd pngs2cel
+go build
 ```
 
 ## Usage
 
-```bash
-# Convert single PNG image into corresponding CEL image.
-$GOPATH/bin/pngs2cel -o panel8.cel -pal_path /path/to/town.pal panel8.png
-```
+### Single PNG image to CEL
 
 ```bash
-# Convert multiple PNG images into corresponding CEL image.
-$GOPATH/bin/pngs2cel -o health_orb.cel -pal_path /path/to/town.pal health_0001.png health_0002.png health_0003.png
+# Convert single PNG image into a corresponding CEL image.
+./pngs2cel -o panel8.cel -pal_path /path/to/town.pal panel8.png
+```
+
+### Multiple PNG images to CEL
+
+```bash
+# Convert multiple PNG images into a corresponding CEL image.
+./pngs2cel -o health_orb.cel -pal_path /path/to/town.pal health_0001.png health_0002.png health_0003.png
+```
+
+![Custom health and mana orb graphics](inc/cel.png "Custom health and mana orb graphics")
+
+### Multiple PNG images to CL2
+
+```bash
+# Convert multiple PNG images into a corresponding CL2 image.
+./pngs2cel -cl2 -o portal2.cl2 -pal_path /path/to/town.pal portal_*.png
+```
+
+![Custom town portal graphics](inc/cl2.png "Custom town portal graphics")
+
+### Multiple PNG images for multiple direction to CL2 archive
+
+```bash
+# Convert multiple PNG images into a corresponding CL2 image.
+./pngs2cel -cl2_archive -o cow.cl2 -pal_path /path/to/town.pal dir1/ dir2/ dir3/ dir4/ dir5/ dir6/ dir7/ dir8/
 ```
--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/mewkiz/pkg/imgutil"
@@ -18,7 +20,9 @@ import (
 
 func usage() {
 	const use = `
-Usage: pngs2cel [OPTIONS]... FILE.png...`
+Usage:
+   pngs2cel [OPTIONS]... FILE.png...
+   pngs2cel -cl2_archive [OPTIONS]... DIR...`
 	fmt.Fprintln(os.Stderr, use[1:])
 	flag.PrintDefaults()
 }
@@ -27,66 +31,114 @@ func main() {
 	// Parse command line arguments.
 	var (
 		// Store output in CL2 format.
-		cl2 bool
+		cl2Flag bool
+		// Store output in CL2 archive format.
+		cl2ArchiveFlag bool
 		// CEL image output path.
 		output string
 		// Path to levels/towndata/town.pal.
 		palPath string
 	)
-	flag.BoolVar(&cl2, "cl2", false, "store output in Cl2 format")
-	flag.StringVar(&output, "o", "output.cel", "CEL image output path")
+	flag.BoolVar(&cl2Flag, "cl2", false, "store output in CL2 format")
+	flag.BoolVar(&cl2ArchiveFlag, "cl2_archive", false, "store output in CL2 archive format")
+	flag.StringVar(&output, "o", "output.cel", "CEL or CL2 image output path")
 	flag.StringVar(&palPath, "pal_path", "town.pal", "path to levels/towndata/town.pal")
 	flag.Usage = usage
 	flag.Parse()
-	pngPaths := flag.Args()
-	if len(pngPaths) < 1 {
+	paths := flag.Args()
+	if len(paths) < 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
-	// Convert PNG images to a single CEL image.
-	if err := pngs2cel(pngPaths, output, palPath, cl2); err != nil {
-		log.Fatalf("%+v", err)
+	switch {
+	case cl2ArchiveFlag:
+		dirs := paths
+		cl2Archive, err := dirs2CL2Archive(dirs, output, palPath)
+		if err != nil {
+			log.Fatalf("%+v", err)
+		}
+		if err := dumpCL2Archive(cl2Archive, output); err != nil {
+			log.Fatalf("%+v", err)
+		}
+	default:
+		// Convert PNG images to a single CEL image.
+		pngPaths := paths
+		celImg, err := pngs2CEL(pngPaths, output, palPath, cl2Flag, false)
+		if err != nil {
+			log.Fatalf("%+v", err)
+		}
+		// Write CEL image to file.
+		if cl2Flag && strings.HasSuffix(output, ".cel") {
+			output = strings.TrimSuffix(output, ".cel") + ".cl2"
+		}
+		if err := dumpCEL(celImg, output); err != nil {
+			log.Fatalf("%+v", err)
+		}
 	}
 }
 
-// pngs2cel converts the given PNG images to a single CEL image, writing the
+// dirs2CL2Archive converts the PNG images found in the given directories to a
+// single CL2 archive, writing to output CL2 archive file to the specified
+// output path and parsing the town.pal colour palette from the specified PAL
+// path.
+func dirs2CL2Archive(dirs []string, output, palPath string) (*CL2Archive, error) {
+	// Convert the PNG files contained within each directory to a corresponding CEL
+	// file.
+	var celImgs []*CELImage
+	for _, dir := range dirs {
+		pngPaths, err := findFilesInDir(dir)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		const cl2Flag = true
+		celImg, err := pngs2CEL(pngPaths, output, palPath, cl2Flag, true)
+		if err != nil {
+			log.Fatalf("%+v", err)
+		}
+		celImgs = append(celImgs, celImg)
+	}
+	// Pack CEL files in CL2 archive.
+	cl2Archive := &CL2Archive{
+		celImgs: celImgs,
+	}
+	return cl2Archive, nil
+}
+
+// CL2Archive is a CL2 archive containing multiple CEL files.
+type CL2Archive struct {
+	// CEL files contained within CL2 archive.
+	celImgs []*CELImage
+}
+
+// pngs2CEL converts the given PNG images to a single CEL image, writing the
 // output CEL file to the specified output path and parsing the town.pal colour
 // palette from the specified PAL path.
-func pngs2cel(pngPaths []string, output, palPath string, cl2 bool) error {
+func pngs2CEL(pngPaths []string, output, palPath string, cl2Flag, cl2ArchiveFlag bool) (*CELImage, error) {
 	// Parse town.pal.
 	pal, err := parsePal(palPath)
 	if err != nil {
-		return errors.WithStack(err)
+		return nil, errors.WithStack(err)
 	}
 	// Parse PNG images.
 	var imgs []image.Image
 	for _, pngPath := range pngPaths {
 		img, err := imgutil.ReadFile(pngPath)
 		if err != nil {
-			return errors.WithStack(err)
+			return nil, errors.WithStack(err)
 		}
 		imgs = append(imgs, img)
 	}
 	// Convert PNG images to CEL image.
-	var celImg celImage
 	switch {
-	case cl2:
-		celImg = createCl2(imgs, pal)
+	case cl2Flag, cl2ArchiveFlag:
+		return createCL2(imgs, pal, cl2ArchiveFlag), nil
 	default:
-		celImg = createCel(imgs, pal)
+		return createCEL(imgs, pal), nil
 	}
-	// Write CEL image to file.
-	if cl2 && strings.HasSuffix(output, ".cel") {
-		output = strings.TrimSuffix(output, ".cel") + ".cl2"
-	}
-	if err := dumpCel(celImg, output); err != nil {
-		return errors.WithStack(err)
-	}
-	return nil
 }
 
-// celImage is a CEL image containing a set of image frames.
-type celImage struct {
+// CELImage is a CEL image containing a set of image frames.
+type CELImage struct {
 	// Number of frames.
 	nframes uint32
 	// Offset to each frame.
@@ -98,9 +150,9 @@ type celImage struct {
 	frames [][]byte // [nframes]Frame
 }
 
-// createCel creates a CEL image based on the given image frames and colour
+// createCEL creates a CEL image based on the given image frames and colour
 // palette.
-func createCel(imgs []image.Image, pal color.Palette) celImage {
+func createCEL(imgs []image.Image, pal color.Palette) *CELImage {
 	nframes := len(imgs)
 	var frames [][]byte
 	for _, img := range imgs {
@@ -112,7 +164,7 @@ func createCel(imgs []image.Image, pal color.Palette) celImage {
 	for i, frame := range frames {
 		frameOffsets[i+1] = uint32(len(frame)) + frameOffsets[i]
 	}
-	celImg := celImage{
+	celImg := &CELImage{
 		nframes:      uint32(nframes),
 		frameOffsets: frameOffsets,
 		frames:       frames,
@@ -120,13 +172,18 @@ func createCel(imgs []image.Image, pal color.Palette) celImage {
 	return celImg
 }
 
-// createCl2 creates a CL2 image based on the given image frames and colour
+// createCL2 creates a CL2 image based on the given image frames and colour
 // palette.
-func createCl2(imgs []image.Image, pal color.Palette) celImage {
+func createCL2(imgs []image.Image, pal color.Palette, cl2ArchiveFlag bool) *CELImage {
 	nframes := len(imgs)
 	var frames [][]byte
 	for _, img := range imgs {
-		frame := getCl2Frame(img, pal)
+		var frame []byte
+		if cl2ArchiveFlag {
+			frame = getCL2EmbeddedFrame(img, pal)
+		} else {
+			frame = getCL2Frame(img, pal)
+		}
 		header := []byte{0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 		frame = append(header, frame...)
 		frames = append(frames, frame)
@@ -136,7 +193,7 @@ func createCl2(imgs []image.Image, pal color.Palette) celImage {
 	for i, frame := range frames {
 		frameOffsets[i+1] = uint32(len(frame)) + frameOffsets[i]
 	}
-	celImg := celImage{
+	celImg := &CELImage{
 		nframes:      uint32(nframes),
 		frameOffsets: frameOffsets,
 		frames:       frames,
@@ -193,9 +250,110 @@ func getCelFrame(img image.Image, pal color.Palette) []byte {
 	return frame
 }
 
-// getCl2Frame converts the given image to the corresponding CL2 frame contents,
+// rleEncode returns an RLE-encoded version of the given pixels.
+func rleEncode(pixels []byte) []byte {
+	var buf []byte
+	var i int
+	start := 0
+	for i = 0; i < len(pixels); {
+		n := runLength(pixels[i:])
+		if n >= 3 {
+			// store regular pixels.
+			if len(pixels[start:i]) > 0 {
+				m := len(pixels[start:i])
+				cmd := uint8(int8(-m))
+				buf = append(buf, cmd)
+				buf = append(buf, pixels[start:i]...)
+				start = i
+			}
+			// store RLE-encoded pixels.
+			idx := pixels[i]
+			cmd := uint8(int8(-(n + 65)))
+			buf = append(buf, cmd)
+			buf = append(buf, idx)
+			i += n
+			start = i
+		} else {
+			i++
+		}
+	}
+	// store regular pixels.
+	if len(pixels[start:i]) > 0 {
+		m := len(pixels[start:i])
+		cmd := uint8(int8(-m))
+		buf = append(buf, cmd)
+		buf = append(buf, pixels[start:i]...)
+	}
+	return buf
+}
+
+func runLength(pixels []byte) int {
+	n := 1
+	b := pixels[0]
+	for j := 1; j < len(pixels); j++ {
+		if pixels[j] != b {
+			return n
+		}
+		n++
+	}
+	return n
+}
+
+// getCL2EmbeddedFrame converts the given image to the corresponding CL2 frame
+// contents (as embedded within a CL2 archive), using the specified palette for
+// colours.
+func getCL2EmbeddedFrame(img image.Image, pal color.Palette) []byte {
+	bounds := img.Bounds()
+	var frame []byte
+	ntrans := 0       // transparent pixels.
+	var pixels []byte // regular pixels.
+	// Set regular pixels.
+	setRegular := func() {
+		buf := rleEncode(pixels)
+		frame = append(frame, buf...)
+		//cmd := byte(-len(pixels))
+		//frame = append(frame, cmd)
+		//frame = append(frame, pixels...)
+		pixels = pixels[:0] // reset pixel buffer.
+	}
+	// Set transparent pixels.
+	setTrans := func() {
+		t := byte(ntrans)
+		frame = append(frame, t)
+		ntrans = 0
+	}
+	for y := bounds.Max.Y - 1; y >= 0; y-- {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			c := img.At(x, y)
+			if isTransparent(c) {
+				if len(pixels) > 0 {
+					setRegular()
+				}
+				ntrans++
+			} else {
+				if ntrans > 0 {
+					setTrans()
+				}
+				idx := byte(pal.Index(c))
+				pixels = append(pixels, idx)
+			}
+			// -1 through -65
+			if len(pixels) >= 65 { // TODO: double check; should be `len(pixels) >= 64`?
+				setRegular()
+				continue
+			}
+			if ntrans >= 0x7F {
+				setTrans()
+				continue
+			}
+		}
+	}
+	return frame
+}
+
+// getCL2Frame converts the given image to the corresponding CL2 frame contents,
 // using the specified palette for colours.
-func getCl2Frame(img image.Image, pal color.Palette) []byte {
+func getCL2Frame(img image.Image, pal color.Palette) []byte {
 	bounds := img.Bounds()
 	var frame []byte
 	ntrans := 0       // transparent pixels.
@@ -243,9 +401,9 @@ func getCl2Frame(img image.Image, pal color.Palette) []byte {
 	return frame
 }
 
-// dumpCel writes the given CEL image in binary format to the specified output
+// dumpCEL writes the given CEL image in binary format to the specified output
 // path.
-func dumpCel(celImg celImage, output string) error {
+func dumpCEL(celImg *CELImage, output string) error {
 	f, err := os.Create(output)
 	if err != nil {
 		return errors.WithStack(err)
@@ -259,6 +417,105 @@ func dumpCel(celImg celImage, output string) error {
 	for _, frame := range celImg.frames {
 		if _, err := f.Write(frame); err != nil {
 			return errors.WithStack(err)
+		}
+	}
+	return nil
+}
+
+// dumpCL2Archive writes the given CL2 archive in binary format to the specified
+// output path.
+func dumpCL2Archive(cl2Archive *CL2Archive, output string) error {
+	f, err := os.Create(output)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	// CL2Archive format:
+	//
+	//    type CL2Archive struct {
+	//       // CL2 archive header.
+	//       Hdr CL2ArchiveHeader
+	//       // CEL headers.
+	//       CELHdrs [ndirs]CELHeader
+	//       // CEL bodies, containing pixel data.
+	//       CELBodies [ndirs]CELBody
+	//    }
+	//
+	//    // Number of directions; one per embedded CEL file.
+	//    const ndirs = 8
+	//
+	//    type CL2ArchiveHeader struct {
+	//       // offset from start of CL2 archive file to CEL header start.
+	//       CELHdrOffsets [ndirs]uint32
+	//    }
+	//
+	//    type CELHeader struct {
+	//       // Number of frames in CEL file.
+	//       NFrames uint32
+	//       // CEL body offsets; from start of this CEL header.
+	//       CELBodyOffsets [NFrames+1]uint32
+	//    }
+	//
+	//    type CELBody struct {
+	//       // slab line offsets into pixel data.
+	//       LineOffets [5]uint16
+	//       // RLE-encoded pixel data
+	//       Data []byte
+	//    }
+	const (
+		// Number of directions; one per embedded CEL file.
+		ndirs = 8
+		// 2 bytes.
+		uint16Size = 2
+		// 4 bytes.
+		uint32Size = 4
+	)
+	// Write CL2 archive header.
+	cl2ArchiveHdrSize := ndirs * uint32Size
+	nframes := cl2Archive.celImgs[0].nframes
+	celHdrSize := 1*uint32Size + (nframes+1)*uint32Size
+	var celHdrOffsets [ndirs]uint32
+	offset := uint32(cl2ArchiveHdrSize)
+	for i := range celHdrOffsets {
+		celHdrOffsets[i] = uint32(offset)
+		offset += uint32(celHdrSize)
+	}
+	if err := binary.Write(f, binary.LittleEndian, celHdrOffsets); err != nil {
+		return errors.WithStack(err)
+	}
+	// Write CEL headers.
+	var celBodySizes [ndirs]uint32
+	for i := range celBodySizes {
+		celImg := cl2Archive.celImgs[i]
+		size := 5 * uint16Size // size of slab line offsets into data array.
+		for _, frame := range celImg.frames {
+			size += len(frame)
+		}
+		celBodySizes[i] = uint32(size)
+	}
+	// offset currently at start of first cel body.
+	for i, celImg := range cl2Archive.celImgs {
+		for j := range celImg.frameOffsets {
+			celImg.frameOffsets[j] = offset - celHdrOffsets[i]
+			if j < int(celImg.nframes) {
+				offset += uint32(len(celImg.frames[j]))
+			}
+		}
+	}
+	// Write CEL headers.
+	for _, celImg := range cl2Archive.celImgs {
+		if err := binary.Write(f, binary.LittleEndian, celImg.nframes); err != nil {
+			return errors.WithStack(err)
+		}
+		if err := binary.Write(f, binary.LittleEndian, celImg.frameOffsets); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	// Write CEL bodies.
+	for _, celImg := range cl2Archive.celImgs {
+		for _, frame := range celImg.frames {
+			if _, err := f.Write(frame); err != nil {
+				return errors.WithStack(err)
+			}
 		}
 	}
 	return nil
@@ -309,4 +566,24 @@ func parsePal(palPath string) (color.Palette, error) {
 		}
 	}
 	return pal, nil
+}
+
+// findFilesInDir returns a list of paths to the files located in the given
+// directory.
+func findFilesInDir(dir string) ([]string, error) {
+	var filePaths []string
+	visit := func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		if info.IsDir() {
+			return nil
+		}
+		filePaths = append(filePaths, path)
+		return nil
+	}
+	if err := filepath.Walk(dir, visit); err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return filePaths, nil
 }
