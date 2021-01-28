8dd78ff8964aaca17b6fa51af66242304ece34e5
Robin Eklind <rnd0x00@gmail.com>
2021-01-28 23:02:40 +0100

    wyvern: specify palette path as argument to `all.sh`
    Invoke as `all.sh /path/to/town.pal`
    
    Updates #2.

--- wyvern/all.sh
+++ wyvern/all.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+if [ $# -ne 1 ]; then
+	echo "Usage: all.sh /path/to/town.pal"
+	exit 1
+fi
+
+PAL_PATH=$1
+
 ./download_graphics.sh
 ./split_tiles.sh
 ./copy_graphic.sh
-./gen_cl2.sh
+./gen_cl2.sh ${PAL_PATH}
--- wyvern/gen_cl2.sh
+++ wyvern/gen_cl2.sh
@@ -2,10 +2,21 @@
 
 mkdir -p cl2/
 
-pngs2cel -cl2_archive -o cl2/wyvern_breathe.cl2 -pal_path ~/_share_/diabdat/levels/towndata/town.pal wyvern/wyvern_breathe/wyvern_breathe_{1,2,3,4,5,6,7,8}
-pngs2cel -cl2_archive -o cl2/wyvern_die.cl2 -pal_path ~/_share_/diabdat/levels/towndata/town.pal wyvern/wyvern_die/wyvern_die_{1,2,3,4,5,6,7,8}
-pngs2cel -cl2_archive -o cl2/wyvern_fly.cl2 -pal_path ~/_share_/diabdat/levels/towndata/town.pal wyvern/wyvern_fly/wyvern_fly_{1,2,3,4,5,6,7,8}
-pngs2cel -cl2_archive -o cl2/wyvern_hit.cl2 -pal_path ~/_share_/diabdat/levels/towndata/town.pal wyvern/wyvern_hit/wyvern_hit_{1,2,3,4,5,6,7,8}
-pngs2cel -cl2_archive -o cl2/wyvern_hover.cl2 -pal_path ~/_share_/diabdat/levels/towndata/town.pal wyvern/wyvern_hover/wyvern_hover_{1,2,3,4,5,6,7,8}
-pngs2cel -cl2_archive -o cl2/wyvern_ram.cl2 -pal_path ~/_share_/diabdat/levels/towndata/town.pal wyvern/wyvern_ram/wyvern_ram_{1,2,3,4,5,6,7,8}
-pngs2cel -cl2_archive -o cl2/wyvern_sting.cl2 -pal_path ~/_share_/diabdat/levels/towndata/town.pal wyvern/wyvern_sting/wyvern_sting_{1,2,3,4,5,6,7,8}
+if [ $# -ne 1 ]; then
+	echo "Usage: gen_cl2.sh /path/to/town.pal"
+	exit 1
+fi
+
+PAL_PATH=$1
+
+# Add pngs2cel repository root to PATH for the duration of this script.
+PATH=../:$PATH
+
+
+pngs2cel -cl2_archive -o cl2/wyvern_breathe.cl2 -pal_path ${PAL_PATH} wyvern/wyvern_breathe/wyvern_breathe_{1,2,3,4,5,6,7,8}
+pngs2cel -cl2_archive -o cl2/wyvern_die.cl2 -pal_path ${PAL_PATH} wyvern/wyvern_die/wyvern_die_{1,2,3,4,5,6,7,8}
+pngs2cel -cl2_archive -o cl2/wyvern_fly.cl2 -pal_path ${PAL_PATH} wyvern/wyvern_fly/wyvern_fly_{1,2,3,4,5,6,7,8}
+pngs2cel -cl2_archive -o cl2/wyvern_hit.cl2 -pal_path ${PAL_PATH} wyvern/wyvern_hit/wyvern_hit_{1,2,3,4,5,6,7,8}
+pngs2cel -cl2_archive -o cl2/wyvern_hover.cl2 -pal_path ${PAL_PATH} wyvern/wyvern_hover/wyvern_hover_{1,2,3,4,5,6,7,8}
+pngs2cel -cl2_archive -o cl2/wyvern_ram.cl2 -pal_path ${PAL_PATH} wyvern/wyvern_ram/wyvern_ram_{1,2,3,4,5,6,7,8}
+pngs2cel -cl2_archive -o cl2/wyvern_sting.cl2 -pal_path ${PAL_PATH} wyvern/wyvern_sting/wyvern_sting_{1,2,3,4,5,6,7,8}
