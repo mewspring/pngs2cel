912db7de1394a434892f2cdcceaa97b46f12ccce
galaxyhaxz <sorrows.passage@protonmail.com>
2021-03-12 08:59:26 -0600

    Add alpha rgb support

--- main.go
+++ main.go
@@ -32,6 +32,8 @@ var (
 	useCIE2000 bool
 	// Threshold amount for Euclidean method.
 	useThreshold int
+	// Transparent colour value.
+	nColorKey int
 )
 
 func main() {
@@ -50,6 +52,7 @@ func main() {
 	flag.BoolVar(&cl2ArchiveFlag, "cl2_archive", false, "store output in CL2 archive format")
 	flag.BoolVar(&useCIE2000, "cie2000", false, "use CIE Delta E 2000 instead of Euclidean colour conversion")
 	flag.IntVar(&useThreshold, "threshold", 0, "threshold amount for Euclidean colour conversion")
+	flag.IntVar(&nColorKey, "col_key", -1, "manually specify RGB value of transparent colour")
 	flag.StringVar(&output, "o", "output.cel", "CEL or CL2 image output path")
 	flag.StringVar(&palPath, "pal_path", "town.pal", "path to levels/towndata/town.pal")
 	flag.Usage = usage
@@ -536,8 +539,21 @@ func dumpCL2Archive(cl2Archive *CL2Archive, output string) error {
 
 // isTransparent reports whether the given colour is transparent.
 func isTransparent(c color.Color) bool {
-	_, _, _, a := c.RGBA()
-	return a < 32768 // treat < 50% alpha as transparent.
+	r, g, b, a := c.RGBA()
+	if a < 32768 { // treat < 50% alpha as transparent.
+		return true
+	}
+
+	if nColorKey >= 0 { // user-specified alpha RGB
+		rr := (nColorKey >> 16) & 0xFF
+		gg := (nColorKey >> 8) & 0xFF
+		bb := nColorKey & 0xFF
+		if int(r) == rr && int(g) == gg && int(b) == bb {
+			return true
+		}
+	}
+
+	return false
 }
 
 // parsePal parses the given PAL file and returns the corresponding palette.
